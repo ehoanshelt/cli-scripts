b52d06eb87343a9916dd22122669f0da070b8091
wpe-onboarding <matthew@wpengine.com>
2014-07-08 10:15:21 -0500

    updating ssl.py

--- ssl.py
+++ ssl.py
@@ -9,7 +9,7 @@ This script will log into a pod and check all of the .conf files in /nas/wp/conf
 
 """
 
-sshname = "yourloginhere_" # replace with your login
+sshname = "yourusername_" # replace with your login
 
 if __name__ == "__main__":
     # Check for parameters: first parameter should be a pod/cluster; subsequent parameters should be a list of installs
@@ -20,5 +20,7 @@ if __name__ == "__main__":
     execstr = "ssh " + sshname + "@pod-" + args[1] + ".wpengine.com \"cd /nas/wp/conf/lb/sites; grep :443 *.conf\""
     print "\nList of sites that have a dedicated IP - check this against your migration list:\n"
     call(execstr, shell=True)
+    redirectstr = "ssh " + sshname + "@pod-" + args[1] + ".wpengine.com \"cd /nas/wp/conf/lb/sites; grep -A 5 \'Redirect insecure\' *.conf\""
+    print "\nList of sites that have forced paths to HTTPS - check this against your migration list:\n"
+    call(redirectstr, shell=True)
 
-    
