8309a7fb1cb7164ccefe1fa39421b37638687dcc
scottyloewenjr <scotty.loewen.jr@gmail.com>
2014-09-19 15:51:37 -0500

    Update LRA.py
    eho update, checks for core mods (but there is a bug)

--- LRA.py
+++ LRA.py
@@ -4,8 +4,7 @@ import pickle
 import os
 from subprocess import Popen, PIPE, call
 
-sshname = "yoursshusername" # replace with your login
-localname = "yourlocalfilename" # replace with your login
+sshname = "tturkleton_" # replace with your login
 
 if __name__ == "__main__":
     # Check for parameters: first parameter should be a pod/cluster; subsequent parameters should be a list of installs
@@ -18,164 +17,68 @@ if __name__ == "__main__":
     if len(args[1]) > 3:
     	connstr = "ssh -t %s@pod-%s.wpengine.com" % (sshname,args[1])
 
-    localdir = '/Users/%s/Documents/LRA/%s' % (localname, args[2])
-    successmessage = 'Looks as clean as a whistle!'
-
 #################### Check for Select * in Themes and PLugins #############################
     
-    commandstrthemes = " \"grep -r 'Select \*' /nas/wp/www/sites/%s/wp-content/themes\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstrthemes)
+    commandstrthemes = "%s \"grep -r 'Select \*' /nas/wp/www/sites/%s/wp-content/themes\"" % (connstr, args[2])
     print "Checking for Bad Queries in Themes:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! Bad Queries found in themes!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/bad_query_themes.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/bad_query_themes.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+    call(commandstrthemes, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
     
-    commandstrplugins = " \"grep -r 'Select \*' /nas/wp/www/sites/%s/wp-content/plugins\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstrplugins)
-    print "Checking for Bad Queries in Plugins:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! Bad Queries found in plugins!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/bad_query_plugins.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/bad_query_plugins.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+    commandstrplugins = "%s \"grep -r 'Select \*' /nas/wp/www/sites/%s/wp-content/plugins\"" % (connstr, args[2])
+    print "Checking for Bad Queries in Themes:\n"
+    call(commandstrplugins, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
+    
     
 #################### Check for SESSION Variables in Themes and PLugins #############################
     
-    commandstrsest = " \"grep -r '_SESSION' /nas/wp/www/sites/%s/wp-content/themes\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstrsest)
-    print "Checking for SESSION Variables in Themes:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! SESSION Variables found in themes!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/SESSION_themes.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/SESSION_themes.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+    commandstrsest = "%s \"grep -r '_SESSION' /nas/wp/www/sites/%s/wp-content/themes\"" % (connstr, args[2])
+    print "Checking for SESSIONS in Themes:\n"
+    call(commandstrsest, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
     
-    commandstrsesp = " \"grep -r '_SESSION' /nas/wp/www/sites/%s/wp-content/plugins\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstrsesp)
-    print "Checking for SESSION Variables in Plugins:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! SESSION Variables found in plugins!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/SESSION_plugins.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/SESSION_plugins.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+    commandstrsesp = "%s \"grep -r '_SESSION' /nas/wp/www/sites/%s/wp-content/plugins\"" % (connstr, args[2])
+    print "Checking for SESSIONS in Plugins:\n"
+    call(commandstrsesp, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
         
         
 #################### Check for Upload Manipulation in Themes and PLugins #############################
     
-    commandstruploadt = " \"ack-grep -r 'add_action(\'upload_dir' /nas/wp/www/sites/%s/wp-content/themes\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstruploadt)
-    print "Checking for Upload Manipulation in Themes:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! Upload Manipulation found in themes!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/Upload_Mani_themes.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/Upload_Mani_themes.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+    commandstruploadt = "%s \"grep -r 'add_action(\'upload_dir\'' /nas/wp/www/sites/%s/wp-content/themes\"" % (connstr, args[2])
+    print "Looking for Upload Manipulation is Themes:\n"
+    call(commandstruploadt, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
     
-    commandstruploadp = " \"ack-grep -r 'add_action(\'upload_dir' /nas/wp/www/sites/%s/wp-content/plugins\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstruploadp)
-    print "Checking for Upload Manipulation in Plugins:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! Upload Manipulation found in plugins!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/Upload_Mani_plugins.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/Upload_Mani_plugins.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+    commandstruploadp = "%s \"grep -r 'add_action(\'upload_dir\'' /nas/wp/www/sites/%s/wp-content/plugins\"" % (connstr, args[2])
+    print "Looking for Upload Manipulation is Plugins:\n"
+    call(commandstruploadp, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
 
 #################### Check for Slow SQL Queries #############################
-    sqlslow = "ssh -t %s@pod-%s.wpengine.com \"sudo cat /var/log/mysql/mysql-slow.log | grep %s\"" % (sshname, args[1], args[2])
+    sqlslow = "%s \"sudo cat /var/log/mysql/mysql-slow.log | grep %s\"" % (connstr, args[2])
     print "My-SQL Slow Log:\n"
     call(sqlslow, shell=True)
     print "++++++++++++++++++++++++++++++++++"
 
 #################### Check for Long Query Error #############################
-    longquery = "ssh -t %s@pod-%s.wpengine.com \"sudo cat /var/log/apache2/%s.error.log | grep LONG\"" % (sshname, args[1], args[2])
+    longquery = "%s \"sudo cat /var/log/apache2/%s.error.log | grep LONG\"" % (connstr, args[2])
     print "Long Query Log:\n"
     call(longquery, shell=True)
     print "++++++++++++++++++++++++++++++++++"
-	
-#################### Check for Upload Manipulation in Themes and PLugins #############################
-    sqlslow = "ssh -t %s@pod-%s.wpengine.com \"cat /var/log/mysql/mysql-slow.log | grep ehoanshelt" % (sshname, args[1])
-    print "My-SQL Slow Log:\n"
-    call(sqlslow, shell=True)
 
-#################### Check for php handler in .htaccess #############################
-    
-    commandstrphphandle = " \"grep 'AddType application/x-httpd-php' /nas/wp/www/sites/%s/.htaccess\"" % (args[2])
-    execstr = '%s%s' % (connstr,commandstrphphandle)
-    print "Checking for PHP handler in .htaccess:\n"
-    p = Popen(execstr, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if len(output) > 10:
-        text = "ALERT! PHP handler found in .htaccess!"
-        print text
-        if not os.path.exists(localdir):
-            os.makedirs(localdir)
-        file = open(localdir + '/php_handler.txt', 'w+')
-        pickle.dump(output, file)
-        file.close()
-        print "Writing to File Completed to %s/php_handler.txt" % (localdir)
-        print "++++++++++++++++++++++++++++++++++"
-    else:
-        print successmessage
-        print "++++++++++++++++++++++++++++++++++"
+#################### Check WordPress Core Modifications for wp-admin & includes #############################
+    wpadmincheck = "%s \"cd /nas/wp/www/sites/%s/_wpeprivate && tar -zxf /nas/wp/install/wordpress-3.9.2.tar.gz -C /nas/wp/www/sites/%s/_wpeprivate && diff -r wordpress/wp-admin ../wp-admin\"" % (connstr, args[2],args[2])
+    print "WordPress Core Modidications:\n"
+    call(wpadmincheck, shell=True)
+    wpincludecheck = "%s \"cd /nas/wp/www/sites/%s/_wpeprivate && tar -zxf /nas/wp/install/wordpress-3.9.2.tar.gz -C /nas/wp/www/sites/%s/_wpeprivate && diff -r wordpress/wp-includes ../wp-includes && rm -r wordpress\"" % (connstr, args[2],args[2])
+    call(wpincludecheck, shell=True)
+    print "Finished!"
+    print "++++++++++++++++++++++++++++++++++"
+
